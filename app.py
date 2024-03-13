9d480a56bd2712b92c572e72e08089e3122afe3d
nhansen23 <156024214+nhansen23@users.noreply.github.com>
2024-03-12 21:40:02 -0500

    Update app.py with requested components

--- app.py
+++ app.py
@@ -1,9 +1,85 @@
 import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
+from shiny import render
 import palmerpenguins
 
 # Use the built-in function to load the Palmer Penguins dataset
-penguins_df = palmerpenguins.load_penguins()
+penguins = palmerpenguins.load_penguins()
 
 ui.page_opts(title='Palmer Penguins: Hansen', fillable=True)
+
+# Add a Shiny UI sidebar for user interaction
+# Use the ui.sidebar() function to create a sidebar
+# Set the open parameter to "open" to make the sidebar open by default
+# Use a with block to add content to the sidebar
+with ui.sidebar(open='open'):
+    # Use the ui.h2() function to add a 2nd level header to the sidebar
+    #   pass in a string argument (in quotes) to set the header text to "Sidebar"
+    ui.h2("Sidebar")
+    
+    # Use ui.input_selectize() to create a dropdown input to choose a column
+    #   pass in three arguments:
+    #   the name of the input (in quotes), e.g., "selected_attribute"
+    #   the label for the input (in quotes)
+    #   a list of options for the input (in square brackets) 
+    #   e.g. ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
+    ui.input_selectize("selected_attribute","Attribute",["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"])
+   
+    # Use ui.input_numeric() to create a numeric input for the number of Plotly histogram bins
+    #   pass in two arguments:
+    #   the name of the input (in quotes), e.g. "plotly_bin_count"
+    #   the label for the input (in quotes)
+    ui.input_numeric("plotly_bin_count","Bin Count",5)
+    
+    # Use ui.input_slider() to create a slider input for the number of Seaborn bins
+    #   pass in four arguments:
+    #   the name of the input (in quotes), e.g. "seaborn_bin_count"
+    #   the label for the input (in quotes)
+    #   the minimum value for the input (as an integer)
+    #   the maximum value for the input (as an integer)
+    #   the default value for the input (as an integer)
+    ui.input_slider("seaborn_bin_count","Bin Count",0,20,5)
+
+    # Use ui.input_checkbox_group() to create a checkbox group input to filter the species
+    #   pass in five arguments:
+    #   the name of the input (in quotes), e.g.  "selected_species_list"
+    #   the label for the input (in quotes)
+    #   a list of options for the input (in square brackets) as ["Adelie", "Gentoo", "Chinstrap"]
+    #   a keyword argument selected= a list of selected options for the input (in square brackets)
+    #   a keyword argument inline= a Boolean value (True or False) as you like
+    ui.input_checkbox_group("selected_species_list","Species",["Adelie", "Gentoo", "Chinstrap"],selected=["Adelie", "Gentoo", "Chinstrap"],inline=True)
+    
+    # Use ui.hr() to add a horizontal rule to the sidebar
+    ui.hr()
+
+    # Use ui.a() to add a hyperlink to the sidebar
+    #   pass in two arguments:
+    #   the text for the hyperlink (in quotes), e.g. "GitHub"
+    #   a keyword argument href= the URL for the hyperlink (in quotes), e.g. your GitHub repo URL
+    #   a keyword argument target= "_blank" to open the link in a new tab
+    ui.a("My GitHub Repo",href="https://github.com/nhansen23/cintel-02-data",target="_blank")
+
+# When passing in multiple arguments to a function, separate them with commas.
+
+@render.data_frame
+def penguins_df():
+    return render.DataGrid(penguins)
+        
+with ui.layout_columns(
+    data_grid,
+    data_table,    
+)
+
+
+data_grid = ui.card(
+    ui.card_header("Data Grid Card"),
+    ui.output_plot("data_grid_card"),
+    full_screen=True,
+)
+
+data_table = ui.card(
+    ui.card_header("Data Table Card"),
+    ui.output_plot("data_table_card"),
+    full_screen=True,
+)
